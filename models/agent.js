b7b41fc64d22117b4cd6c2b659422adec9579398
Daniel Bidulock <daniel_bidulock@sil.org>
2021-09-23 16:38:15 -0600

    Get publicAddress property and test

--- models/agent.js
+++ models/agent.js
@@ -1,10 +1,31 @@
 module.exports = function(mongoose) {
+  const ethereum_address = require('ethereum-address');
   const Schema = mongoose.Schema;
 
   const AgentSchema = new Schema({
+    publicAddress: {
+      type: String,
+      trim: true,
+      required: [true, 'Public address required'],
+      empty: [false, 'Public address required'],
+      validate: [
+        {
+          validator: function(val) {
+            return ethereum_address.isAddress(val);
+          },
+          message: 'Invalid public address'
+        },
+        {
+          validator: async function(val) {
+            const count = await this.model('Agent').count({ publicAddress: val });
+            return !count;
+          },
+          message: 'That public address is already registered'
+        }
+      ]
+    },
   }, {
     timestamps: true,
-    strict: false
   });
 
   return AgentSchema;
--- spec/models/agentSpec.js
+++ spec/models/agentSpec.js
@@ -3,9 +3,13 @@ const Agent = db.Agent;
 
 describe('Agent', () => {
 
+  const _profile = {
+    publicAddress: '0x0eDB511d9434452D24a3Eeb47E3d02Fda903A73a',
+  };
+
   let agent;
   beforeEach(() => {
-    agent = new Agent();
+    agent = new Agent(_profile);
   });
 
   afterEach(done => {
@@ -27,6 +31,53 @@ describe('Agent', () => {
         done();
       }).catch(err => {
         done.fail(err);
+      });
+    });
+
+    describe('publicAddress', () => {
+
+      it('is required', done => {
+        Agent.create({..._profile, publicAddress: undefined }).then(obj => {
+          done.fail('This should not have saved');
+        }).catch(error => {
+          expect(Object.keys(error.errors).length).toEqual(1);
+          expect(error.errors['publicAddress'].message).toEqual('Public address required');
+          done();
+        });
+      });
+
+      it('cannot be null', done => {
+        Agent.create({..._profile, publicAddress: null }).then(obj => {
+          done.fail('This should not have saved');
+        }).catch(error => {
+          expect(Object.keys(error.errors).length).toEqual(1);
+          expect(error.errors['publicAddress'].message).toEqual('Public address required');
+          done();
+        });
+      });
+
+      it('is unique', done => {
+        agent.save().then(obj => {
+          Agent.create(_profile).then(obj => {
+            done.fail('This should not have saved');
+          }).catch(error => {
+            expect(Object.keys(error.errors).length).toEqual(1);
+            expect(error.errors['publicAddress'].message).toEqual('That public address is already registered');
+            done();
+          });
+        }).catch(error => {
+          done.fail(error);
+        });
+      });
+
+      it('is valid', done => {
+        Agent.create({..._profile, publicAddress: 'this is not valid' }).then(obj => {
+          done.fail('This should not have saved');
+        }).catch(error => {
+          expect(Object.keys(error.errors).length).toEqual(1);
+          expect(error.errors['publicAddress'].message).toEqual('Invalid public address');
+          done();
+        });
       });
     });
   });
