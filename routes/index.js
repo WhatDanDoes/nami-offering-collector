69c166861192d2dc5b06ec15245879d488f19ae7
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-05 10:01:16 -0600

    Move index view to landing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
  * Landing page
  */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: process.env.TITLE, messages: req.flash(), agent: req.agent });
+  res.render('landing', { title: process.env.TITLE, messages: req.flash(), agent: req.agent });
 });
 
 module.exports = router;
