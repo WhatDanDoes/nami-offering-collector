2a6c93d6e771d33072d5e2cf5437fd2af66f6c71
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-05 16:48:15 -0600

    Get Update button tests

--- tests/e2e/specs/account.spec.js
+++ tests/e2e/specs/account.spec.js
@@ -5,6 +5,7 @@ context('account managment', () => {
   });
 
   beforeEach(() => {
+    cy.task('dropDatabase');
     cy.visit('/');
     cy.contains('Confirm your identity with Metamask').click();
     cy.confirmMetamaskSignatureRequest();
@@ -21,7 +22,35 @@ context('account managment', () => {
     cy.get('form#account-details').should('have.attr', 'action', '/agent?_method=PUT');
     cy.get('form#account-details').should('have.attr', 'method', 'post');
     cy.get('form#account-details input[type="text"][name="name"]').should('not.be.disabled');
-    cy.get('form#account-details button[type="submit"]').should('not.be.disabled');
+    cy.get('form#account-details button#update-account-button[type="submit"]').should('not.be.disabled');
+  });
+
+  describe('Update button', () => {
+
+    it('allows you to set the name field', () => {
+      cy.get('input[type="text"][name="name"]').should('have.value', '');
+      cy.get('input[type="text"][name="name"]').type('Some Guy');
+      cy.get('#update-account-button').click();
+      cy.get('input[type="text"][name="name"]').should('have.value', 'Some Guy');
+    });
+
+    it('allows you to set an empty name field', () => {
+      cy.get('input[type="text"][name="name"]').should('have.value', '');
+      cy.get('input[type="text"][name="name"]').type('Some Guy');
+      cy.get('#update-account-button').click();
+      cy.get('input[type="text"][name="name"]').should('have.value', 'Some Guy');
+      cy.get('input[type="text"][name="name"]').clear();
+      cy.get('#update-account-button').click();
+      cy.get('input[type="text"][name="name"]').should('have.value', '');
+    });
+
+    it('shows a friendly message on success', () => {
+      cy.get('input[type="text"][name="name"]').should('have.value', '');
+      cy.get('input[type="text"][name="name"]').type('Some Guy');
+      cy.get('#update-account-button').click();
+      cy.get('input[type="text"][name="name"]').should('have.value', 'Some Guy');
+      cy.get('.alert.alert-success').contains('Info updated');
+    });
   });
 });
 
