86e67a0963149834f3eb5ee04bfc1584ce7c1cb6
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-05 15:37:45 -0600

    Get account update form

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,12 @@ const router = express.Router();
  * Landing page
  */
 router.get('/', (req, res, next) => {
-  res.render('landing', { title: process.env.TITLE, messages: req.flash(), agent: req.agent });
+  if (req.agent) {
+    res.render('app', { title: process.env.TITLE, messages: req.flash(), agent: req.agent });
+  }
+  else {
+    res.render('landing', { title: process.env.TITLE, messages: req.flash(), agent: req.agent });
+  }
 });
 
 module.exports = router;
--- tests/e2e/specs/account.spec.js
+++ tests/e2e/specs/account.spec.js
@@ -10,11 +10,18 @@ context('account managment', () => {
     cy.confirmMetamaskSignatureRequest();
   });
 
+  it('displays the connected wallet', () => {
+    cy.fetchMetamaskWalletAddress().then(address => {
+      cy.get('body header h1').contains('You are connected with:');
+      cy.get('body header h3').contains(address.toLowerCase());
+    });
+  });
+
   it('presents a form to edit account details', () => {
-    cy.get('form#account-details input#agent-name-field[type="text"]').should('have.attr', 'action', '/agent?_method=PUT');
-    cy.get('form#account-details input#agent-name-field[type="text"]').should('have.attr', 'method', 'post');
-    cy.get('form#account-details input#agent-name-field[type="text"]').should('not.be.disabled');
-    cy.get('form#account-details button#update-account-details[type="submit"]').should('not.be.disabled');
+    cy.get('form#account-details').should('have.attr', 'action', '/agent?_method=PUT');
+    cy.get('form#account-details').should('have.attr', 'method', 'post');
+    cy.get('form#account-details input[type="text"][name="name"]').should('not.be.disabled');
+    cy.get('form#account-details button[type="submit"]').should('not.be.disabled');
   });
 });
 
