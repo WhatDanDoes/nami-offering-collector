cebcbcd68fdf372313866695d9b8a44ca6aecdfb
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-14 12:50:56 -0600

    Get first transaction tests

--- tests/e2e/specs/account.spec.js
+++ tests/e2e/specs/account.spec.js
@@ -43,79 +43,114 @@ context('account managment', () => {
 
     context('balance > 0', () => {
 
-      it('provides a donation form', () => {
-        cy.get('form#send-eth').should('not.be.disabled');
-        cy.get('form#send-eth').should('not.have.attr', 'action');
-        cy.get('form#send-eth').should('not.have.attr', 'method');
-        cy.get('form#send-eth input[type="text"][name="from"]').should('have.value', address.toLowerCase());
-        cy.get('form#send-eth input[type="text"][name="from"]').should('be.disabled');
-        cy.get('form#send-eth input[type="text"][name="to"]').should('have.value', serverAddress);
-        cy.get('form#send-eth input[type="text"][name="to"]').should('be.disabled');
-        cy.get('form#send-eth input#eth-value-input[type="text"][name="value"]').should('have.value', '');
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-      });
+      describe('donation form', () => {
 
-      it('displays the account\'s current balance', () => {
-        cy.get('#wallet-balance').contains(`Max: ${balance}`)
-        cy.get('#wallet-balance').should('not.contain', 'Max: 0.0')
-      });
-
-      it('only allows numerical content in the "amount" text field', () => {
-        cy.get('#eth-value-input').type('-a1b2.c.3d4');
-        cy.get('#eth-value-input').should('have.value', '12.34');
-      });
-
-      it('only allows up to nine decimal places in the "amount"', () => {
-        cy.get('#eth-value-input').type('123.01234567890123456789');
-        cy.get('#eth-value-input').should('have.value', '123.012345678');
-      });
-
-      it('prefixes a zero to the "amount" value when provided a leading decimal', () => {
-        cy.get('form#send-eth input[type="text"][name="to"]').should('be.disabled');
-        cy.get('#eth-value-input').type('.');
-        cy.get('#eth-value-input').should('have.value', '0.');
-      });
-
-      it('displays an estimated gas fee', () => {
-        cy.task('getGasPrice').then(price => {
-          cy.get('#estimated-gas-price').should('be.empty');
-          cy.get('#eth-value-input').type('.');
-          cy.get('#estimated-gas-price').should('be.empty');
-          cy.get('#eth-value-input').type('.1');
-          cy.get('#estimated-gas-price').contains(`Estimated gas fee: ${price * 21000}`);
-          cy.get('#eth-value-input').clear();
-          cy.get('#estimated-gas-price').should('be.empty');
-        });
-      });
-
-      it('does not allow a value greater than what is available in the account (including gas)', () => {
-        cy.task('getGasPrice').then(price => {
-          cy.get('#eth-value-input').type('99.99999999');
+        it('is correctly formatted', () => {
+          cy.get('form#send-eth').should('not.be.disabled');
+          cy.get('form#send-eth').should('not.have.attr', 'action');
+          cy.get('form#send-eth').should('not.have.attr', 'method');
+          cy.get('form#send-eth input[type="text"][name="from"]').should('have.value', address.toLowerCase());
+          cy.get('form#send-eth input[type="text"][name="from"]').should('be.disabled');
+          cy.get('form#send-eth input[type="text"][name="to"]').should('have.value', serverAddress);
+          cy.get('form#send-eth input[type="text"][name="to"]').should('be.disabled');
+          cy.get('form#send-eth input#eth-value-input[type="text"][name="value"]').should('have.value', '');
           cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-          cy.get('form#send-eth #send-eth-button[type="submit"]').contains('Insufficient funds');
-          cy.get('#estimated-gas-price').contains(`Estimated gas fee: ${price * 21000}`);
-          cy.get('#eth-value-input').clear();
-          cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-          cy.get('form#send-eth #send-eth-button[type="submit"]').contains('Send');
-          cy.get('#estimated-gas-price').should('be.empty');
         });
-      });
 
-      it('disables the Send button if amount is 0', () => {
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-        cy.get('#eth-value-input').type('.');
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-        cy.get('#eth-value-input').clear();
-        cy.get('#eth-value-input').type('0');
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-        cy.get('#eth-value-input').clear();
-        cy.get('#eth-value-input').type('.0');
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
-        cy.get('#eth-value-input').clear();
-        cy.get('#eth-value-input').type('.01');
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('not.be.disabled');
-        cy.get('#eth-value-input').clear();
-        cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+        it('displays the account\'s current balance', () => {
+          cy.get('#wallet-balance').contains(`Max: ${balance}`)
+          cy.get('#wallet-balance').should('not.contain', 'Max: 0.0')
+        });
+
+        describe('amount text field', () => {
+
+          it('allows numerical content in the "amount" text field', () => {
+            cy.get('#eth-value-input').type('-a1b2.c.3d4');
+            cy.get('#eth-value-input').should('have.value', '12.34');
+          });
+
+          it('allows up to nine decimal places in the "amount" field', () => {
+            cy.get('#eth-value-input').type('123.01234567890123456789');
+            cy.get('#eth-value-input').should('have.value', '123.012345678');
+          });
+
+          it('prefixes a zero to the "amount" value when provided a leading decimal', () => {
+            cy.get('form#send-eth input[type="text"][name="to"]').should('be.disabled');
+            cy.get('#eth-value-input').type('.');
+            cy.get('#eth-value-input').should('have.value', '0.');
+          });
+
+          it('reveals an estimated gas fee', () => {
+            cy.task('getGasPrice').then(price => {
+              cy.get('#estimated-gas-price').should('be.empty');
+              cy.get('#eth-value-input').type('.');
+              cy.get('#estimated-gas-price').should('be.empty');
+              cy.get('#eth-value-input').type('.1');
+              cy.get('#estimated-gas-price').contains(`Estimated gas fee: ${price * 21000}`);
+              cy.get('#eth-value-input').clear();
+              cy.get('#estimated-gas-price').should('be.empty');
+            });
+          });
+        });
+
+        describe('Send button', () => {
+
+          it('disables if "amount" value greater than what is available in the account (including gas)', () => {
+            cy.task('getGasPrice').then(price => {
+              cy.get('#eth-value-input').type('99.99999999');
+              cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+              cy.get('form#send-eth #send-eth-button[type="submit"]').contains('Insufficient funds');
+              cy.get('#estimated-gas-price').contains(`Estimated gas fee: ${price * 21000}`);
+              cy.get('#eth-value-input').clear();
+              cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+              cy.get('form#send-eth #send-eth-button[type="submit"]').contains('Send');
+              cy.get('#estimated-gas-price').should('be.empty');
+            });
+          });
+
+          it('disables if amount is 0', () => {
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+            cy.get('#eth-value-input').type('.');
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+            cy.get('#eth-value-input').clear();
+            cy.get('#eth-value-input').type('0');
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+            cy.get('#eth-value-input').clear();
+            cy.get('#eth-value-input').type('.0');
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+            cy.get('#eth-value-input').clear();
+            cy.get('#eth-value-input').type('.01');
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('not.be.disabled');
+            cy.get('#eth-value-input').clear();
+            cy.get('form#send-eth #send-eth-button[type="submit"]').should('be.disabled');
+          });
+
+          /**
+           * 2021-10-14
+           *
+           * `ganache` doesn't really let you reset the state of the chain
+           * on every test. Even if it did, `synpress` does not reset the
+           * that state of the Metamask wallet (though there is a task that
+           * does this). In any case, this test is designed so that there
+           * can be a bunch of runs before the relevant accounts are depleted
+           * of eth.
+           */
+          it('transfers eth to the configured address', () => {
+            cy.task('getBalance', 'CONFIGURED').then(result => {
+              balance = result;
+              // Wouldn't this be nice?
+              //expect(balance).to.equal('0.0');
+              cy.get('#eth-value-input').type('1');
+              cy.get('form#send-eth #send-eth-button[type="submit"]').click();
+              cy.confirmMetamaskTransaction();
+              cy.task('getBalance', 'CONFIGURED').then(result => {
+                // As above...
+                //expect(balance).to.equal('1.0');
+                expect(parseFloat(result)).to.equal(parseFloat(balance) + 1);
+              });
+            });
+          });
+        });
       });
     });
 
