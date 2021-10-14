c02253a5db1eabf06b3dbda6de67ff104a656930
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-14 14:51:03 -0600

    Where did this come from?

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use(methodOverride('_method'))
  */
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/agent', agentRouter);
+app.use('/account', agentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
