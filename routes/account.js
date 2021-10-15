2db98d39d045bb91e8fb633c8aeb3ab0b9741a28
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-15 10:58:51 -0600

    Clean up

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const ensureAuthorized = require('../lib/ensureAuthorized');
-const models = require('../models');
 
 /**
  * GET /account
