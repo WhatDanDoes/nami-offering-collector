07a997070d2b7a05a56c08d87e0ac72053743e90
Daniel Bidulock <daniel_bidulock@sil.org>
2021-10-19 14:09:03 -0600

    Get tests for regular account options on GET /account/:address endpoint

--- spec/features/accountSpec.js
+++ spec/features/accountSpec.js
@@ -227,7 +227,6 @@ describe('account management', () => {
 
         it('redirects', done => {
           request(app)
-           .get('/account')
            .get('/account/some-other-public-address')
            .expect('Content-Type', /text/)
            .expect(302)
@@ -549,6 +548,331 @@ describe('account management', () => {
 
                 models.Agent.find({}).then(agents => {
                   expect(agents.length).toEqual(0);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          }).catch(err => {
+            done.fail(err);
+          });
+        });
+      });
+    });
+  });
+
+  describe('PUT /account/:publicAddress', () => {
+
+    let session, agent;
+
+    beforeEach(done => {
+      session = request(app);
+      session
+        .post('/auth/introduce')
+        .send({ publicAddress: _publicAddress })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          if (err) return done.fail(err);
+          ({ publicAddress, typedData } = res.body);
+
+          const signed = sigUtil.signTypedData(ethUtil.toBuffer(_privateAddress), {privateKey: _privateAddress, data: typedData, version: 'V3' });
+
+          session
+            .post('/auth/prove')
+            .send({ publicAddress: _publicAddress, signature: signed })
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .end((err, res) => {
+              if (err) return done.fail(err);
+
+              expect(res.body.message).toEqual('Welcome!');
+
+              models.Agent.findOne({ where: { publicAddress: _publicAddress } }).then(result => {
+                agent = result;
+
+                done();
+              }).catch(err => {
+                done.fail(err);
+              });
+            });
+        });
+    });
+
+    describe('authorized', () => {
+
+      describe('api', () => {
+
+        describe('success', () => {
+
+          it('returns 201 with a friendly message', done => {
+             session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ name: 'Some Guy' })
+              .set('Accept', 'application/json')
+              .expect('Content-Type', /json/)
+              .expect(201)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                expect(res.body.message).toEqual('Info updated');
+                done();
+              });
+          });
+
+          it('updates the database', done => {
+            expect(agent.name).toBeUndefined();
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ name: 'Some Guy' })
+              .set('Accept', 'application/json')
+              .expect('Content-Type', /json/)
+              .expect(201)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].name).toEqual('Some Guy');
+                  expect(agents[0].publicAddress).toEqual(_publicAddress);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+        });
+
+        describe('failure', () => {
+
+          it('does not allow modifying publicAddress', done => {
+            expect(agent.publicAddress).toEqual(_publicAddress);
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ publicAddress: '0x4D8B94b1358DB655aCAdcCF43768b9AbA00b2e74' })
+              .set('Accept', 'application/json')
+              .expect('Content-Type', /json/)
+              .expect(403)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                expect(res.body.message).toEqual('Forbidden');
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].publicAddress).toEqual(_publicAddress);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+
+          it('does not allow modifying nonce', done => {
+            const currentNonce = agent.nonce;
+            const newNonce = Math.floor(Math.random() * 1000000).toString();
+            expect(currentNonce).not.toEqual(newNonce);
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ nonce: newNonce })
+              .set('Accept', 'application/json')
+              .expect('Content-Type', /json/)
+              .expect(403)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                expect(res.body.message).toEqual('Forbidden');
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].nonce).toEqual(currentNonce);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+        });
+      });
+
+      describe('browser', () => {
+
+        describe('success', () => {
+
+          it('redirects', done => {
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ name: 'Some Guy' })
+              .expect('Content-Type', /text/)
+              .expect(302)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                expect(res.headers['location']).toEqual('/account');
+                done();
+              });
+          });
+
+          it('updates the database', done => {
+            expect(agent.name).toBeUndefined();
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ name: 'Some Guy' })
+              .expect('Content-Type', /text/)
+              .expect(302)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].name).toEqual('Some Guy');
+                  expect(agents[0].publicAddress).toEqual(_publicAddress);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+        });
+
+        describe('failure', () => {
+
+          it('does not allow modifying publicAddress', done => {
+            expect(agent.publicAddress).toEqual(_publicAddress);
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ publicAddress: '0x4D8B94b1358DB655aCAdcCF43768b9AbA00b2e74' })
+              .expect('Content-Type', /text/)
+              .expect(403)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].publicAddress).toEqual(_publicAddress);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+
+          it('does not allow modifying nonce', done => {
+            const currentNonce = agent.nonce;
+            const newNonce = Math.floor(Math.random() * 1000000).toString();
+            expect(currentNonce).not.toEqual(newNonce);
+            session
+              .put(`/account/${agent.publicAddress}`)
+              .send({ nonce: newNonce })
+              .expect('Content-Type', /html/)
+              .expect(403)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                models.Agent.find({}).then(agents => {
+                  expect(agents.length).toEqual(1);
+                  expect(agents[0].nonce).toEqual(currentNonce);
+
+                  done();
+                }).catch(err => {
+                  done.fail(err);
+                });
+              });
+          });
+        });
+      });
+    });
+
+    describe('unauthorized', () => {
+
+      let anotherAgent;
+      beforeEach(done => {
+        models.Agent.create({ publicAddress: '0xE40153f2428846Ce1FFB7B4169ce08c9374b1187' }).then(result => {
+          anotherAgent = result;
+          done();
+        }).catch(err => {
+          done.fail(err);
+        });
+      });
+
+      describe('api', () => {
+
+        it('returns 401 with a friendly message', done => {
+          session
+            .put(`/account/${anotherAgent}`)
+            .send({ name: 'Some Guy' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(401)
+            .end((err, res) => {
+              if (err) return done.fail(err);
+
+              expect(res.body.message).toEqual('Unauthorized');
+              done();
+            });
+        });
+
+        it('does not modify the database', done => {
+          expect(anotherAgent.name).toBeUndefined();
+          session
+            .put(`/account/${anotherAgent}`)
+            .send({ name: 'Some Guy' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(401)
+            .end((err, res) => {
+              if (err) return done.fail(err);
+
+              models.Agent.findOne({ publicAddress: anotherAgent.publicAddress }).then(agent => {
+                expect(agent.name).toBeUndefined();
+
+                done();
+              }).catch(err => {
+                done.fail(err);
+              });
+            });
+        });
+      });
+
+      describe('browser', () => {
+
+        it('redirects', done => {
+          session
+            .put(`/account/${anotherAgent}`)
+            .send({ name: 'Some Guy' })
+            .expect('Content-Type', /text/)
+            .expect(302)
+            .end((err, res) => {
+              if (err) return done.fail(err);
+
+              expect(res.headers['location']).toEqual('/');
+              done();
+            });
+        });
+
+        it('does not modify the database', done => {
+          models.Agent.findOne({ publicAddress: anotherAgent.publicAddress }).then(agent => {
+            expect(agent.name).toBeUndefined();
+
+            session
+              .put(`/account/${anotherAgent}`)
+              .send({ name: 'Some Guy' })
+              .expect('Content-Type', /text/)
+              .expect(302)
+              .end((err, res) => {
+                if (err) return done.fail(err);
+
+                models.Agent.findOne({ publicAddress: anotherAgent.publicAddress }).then(agent => {
+                  expect(agent.name).toBeUndefined();
 
                   done();
                 }).catch(err => {
