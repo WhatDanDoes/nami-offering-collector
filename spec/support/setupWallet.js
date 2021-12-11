5a93f9ef4a19001eb5457f5f42e7d0dc4388ab6f
Daniel Bidulock <daniel_bidulock@sil.org>
2021-12-11 12:01:15 -0700

    Hello, setupWallet.js

---
+++ spec/support/setupWallet.js
@@ -0,0 +1,15 @@
+
+const fs = require('fs');
+const cardanoUtils = require('cardano-crypto.js');
+
+async function setupWallet() {
+  const mnemonic = 'logic easily waste eager injury oval sentence wine bomb embrace gossip supreme'
+  const parentWalletSecret = await cardanoUtils.mnemonicToRootKeypair(mnemonic, 1)
+  const parentWalletPublicExt = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 128));
+  const parentWalletPublic = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 96));
+  const signingMessage = fs.readFileSync('./message.txt', 'utf8');
+
+  return { parentWalletSecret, parentWalletPublic, signingMessage };
+};
+
+module.exports = setupWallet;
