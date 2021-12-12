5ad179fbaea5aeb911c9eabe58267f4753291a97
Daniel Bidulock <daniel_bidulock@sil.org>
2021-12-11 19:33:48 -0700

    Make wallet setup configurable

--- spec/support/setupWallet.js
+++ spec/support/setupWallet.js
@@ -2,14 +2,13 @@
 const fs = require('fs');
 const cardanoUtils = require('cardano-crypto.js');
 
-async function setupWallet() {
-  const mnemonic = 'logic easily waste eager injury oval sentence wine bomb embrace gossip supreme'
+async function setupWallet(mnemonic = 'logic easily waste eager injury oval sentence wine bomb embrace gossip supreme') {
   const parentWalletSecret = await cardanoUtils.mnemonicToRootKeypair(mnemonic, 1)
   const parentWalletPublicExt = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 128));
   const parentWalletPublic = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 96));
   const signingMessage = fs.readFileSync('./message.txt', 'utf8');
 
-  return { parentWalletSecret, parentWalletPublic, signingMessage };
+  return { parentWalletSecret, parentWalletPublic, signingMessage, parentWalletPublicExt };
 };
 
 module.exports = setupWallet;
