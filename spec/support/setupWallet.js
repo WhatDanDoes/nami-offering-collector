928be54cc5620cedae9da1d4879038a1a5128ab4
Daniel Bidulock <daniel_bidulock@sil.org>
2021-12-17 15:11:09 -0700

    Get wallet setup as prescribed by docs

--- spec/support/setupWallet.js
+++ spec/support/setupWallet.js
@@ -1,14 +1,58 @@
-
+/**
+ * 2021-12-16
+ *
+ * From: https://github.com/Emurgo/cardano-serialization-lib/blob/master/doc/getting-started/generating-keys.md
+ */
 const fs = require('fs');
 const cardanoUtils = require('cardano-crypto.js');
 
-async function setupWallet(mnemonic = 'logic easily waste eager injury oval sentence wine bomb embrace gossip supreme') {
-  const parentWalletSecret = await cardanoUtils.mnemonicToRootKeypair(mnemonic, 1)
-  const parentWalletPublicExt = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 128));
-  const parentWalletPublic = cardanoUtils.bech32.encode('addr_test', parentWalletSecret.slice(64, 96));
+const cardano = require('@emurgo/cardano-serialization-lib-nodejs');
+const bip39 = require('bip39');
+
+function harden(num) {
+  return 0x80000000 + num;
+}
+
+function setupWallet(mnemonic = 'logic easily waste eager injury oval sentence wine bomb embrace gossip supreme') {
+
+  const entropy = bip39.mnemonicToEntropy(mnemonic)
+  const rootKey = cardano.Bip32PrivateKey.from_bip39_entropy(
+    Buffer.from(entropy, 'hex'),
+    Buffer.from(''),
+  );
+
+  const accountKey = rootKey
+    .derive(harden(1852)) // purpose
+    .derive(harden(1815)) // coin type
+    .derive(harden(0)); // account #0
+
+  const utxoPubKey = accountKey
+    .derive(0) // external
+    .derive(0)
+    .to_public();
+
+  const stakeKey = accountKey
+    .derive(2) // chimeric
+    .derive(0)
+    .to_public();
+
+  const baseAddr = cardano.BaseAddress.new(
+    cardano.NetworkInfo.mainnet().network_id(),
+    cardano.StakeCredential.from_keyhash(utxoPubKey.to_raw_key().hash()),
+    cardano.StakeCredential.from_keyhash(stakeKey.to_raw_key().hash()),
+  );
+
+  const parentWalletSecret = rootKey.to_bech32();
+
+  //const parentWalletPublic = baseAddr.to_address().to_bech32();
+  const parentWalletPublic = Buffer.from(baseAddr.to_address().to_bytes()).toString('hex');
   const signingMessage = fs.readFileSync('./message.txt', 'utf8');
 
-  return { parentWalletSecret, parentWalletPublic, signingMessage, parentWalletPublicExt };
+  return {
+    parentWalletSecret,
+    parentWalletPublic,
+    signingMessage,
+  };
 };
 
 module.exports = setupWallet;
